b0a755ff131fcc2eeb9aa01103acf47cb920c597
Greg Conen <gregconen@gmail.com>
2015-05-14 17:57:38 -0700

    add activation columns to users table, activate current users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150514234907) do
+ActiveRecord::Schema.define(version: 20150515005059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,11 +59,13 @@ ActiveRecord::Schema.define(version: 20150514234907) do
   add_index "tracks", ["title"], name: "index_tracks_on_title", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",           null: false
-    t.string   "password_digest", null: false
-    t.string   "session_token",   null: false
+    t.string   "email",                            null: false
+    t.string   "password_digest",                  null: false
+    t.string   "session_token",                    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "activated",        default: false
+    t.string   "actication_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
