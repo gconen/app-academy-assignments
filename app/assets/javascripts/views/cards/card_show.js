850f33daf810ea1b6ef835098b829896b2d08991
App Academy Student <student@appacademy.io>
2015-06-08 11:56:21 -0700

    persist drag-and-drop sorting of cards to database

--- app/assets/javascripts/views/cards/card_show.js
+++ app/assets/javascripts/views/cards/card_show.js
@@ -1,6 +1,11 @@
 TrelloClone.Views.CardShow = Backbone.View.extend({
   tagName: "li",
   className: "card",
+  attributes: function () {
+    return {
+      "data-card-id": this.model.get("id"),
+    };
+  },
 
   template: JST['cards/show'],
 
--- app/assets/javascripts/views/lists/list_show.js
+++ app/assets/javascripts/views/lists/list_show.js
@@ -1,6 +1,9 @@
 TrelloClone.Views.ListShow = Backbone.CompositeView.extend({
   tagName: "li",
   className: "list",
+  attributes: function () {
+    return { "data-list-id": this.model.get("id") };
+  },
 
   template: JST['lists/show'],
 
@@ -11,15 +14,24 @@ TrelloClone.Views.ListShow = Backbone.CompositeView.extend({
     this.addCards();
   },
 
+  events: {
+    "sortstop": "sort"
+  },
+
   addCards: function() {
     this.model.cards().each(this.addCard, this);
   },
 
   addCard: function(card) {
+    var cardView = new TrelloClone.Views.CardShow({ model: card });
     this.addSubview(
       ".cards-list",
-      new TrelloClone.Views.CardShow({ model: card })
+      cardView
       );
+    var ord = this.subviews(".cards-list").value().length;
+    card.set({ ord: ord });
+    card.save();
+    cardView.$el.attr("data-ord", ord);
   },
 
 
@@ -28,7 +40,20 @@ TrelloClone.Views.ListShow = Backbone.CompositeView.extend({
     this.attachSubviews();
 
     return this;
-  }
+  },
+
+  sort: function (event, ui) {
+    var $card = ui.item;
+    var card = this.model.cards().get($card.data("card-id"));
+    var $prev = $card.prev();
+    var $next = $card.next();
+    var prevOrd = ( $prev.length > 0 ? $prev.data("ord") : 0 );
+    var nextOrd = ( $next.length > 0 ? $next.data("ord") : prevOrd + 2 );
+    var ord = prevOrd + ((nextOrd - prevOrd) / 2.0);
+    card.set({ord: ord});
+    card.save();
+    $card.attr("data-ord", ord);
+  },
 
 
 });
